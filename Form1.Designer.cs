ba2f6aa0dba37b7faacdf68f5a2a3f66333d8841
SofiaFish <37893374+SofiaFish@users.noreply.github.com>
2019-06-04 14:07:06 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,283 @@
+﻿namespace Galery
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbFolder = new System.Windows.Forms.TextBox();
+            this.bSearch = new System.Windows.Forms.Button();
+            this.lbFiles = new System.Windows.Forms.ListBox();
+            this.bLeft = new System.Windows.Forms.Button();
+            this.bRight = new System.Windows.Forms.Button();
+            this.pbImage = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.rb1 = new System.Windows.Forms.RadioButton();
+            this.rb2 = new System.Windows.Forms.RadioButton();
+            this.rb3 = new System.Windows.Forms.RadioButton();
+            this.rb4 = new System.Windows.Forms.RadioButton();
+            this.rb5 = new System.Windows.Forms.RadioButton();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbComment = new System.Windows.Forms.TextBox();
+            this.bAdd = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.lbComments = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Folder";
+            // 
+            // tbFolder
+            // 
+            this.tbFolder.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbFolder.Location = new System.Drawing.Point(65, 19);
+            this.tbFolder.Name = "tbFolder";
+            this.tbFolder.Size = new System.Drawing.Size(344, 22);
+            this.tbFolder.TabIndex = 1;
+            // 
+            // bSearch
+            // 
+            this.bSearch.Location = new System.Drawing.Point(415, 19);
+            this.bSearch.Name = "bSearch";
+            this.bSearch.Size = new System.Drawing.Size(29, 22);
+            this.bSearch.TabIndex = 2;
+            this.bSearch.Text = "...";
+            this.bSearch.UseVisualStyleBackColor = true;
+            this.bSearch.Click += new System.EventHandler(this.bSearch_Click);
+            // 
+            // lbFiles
+            // 
+            this.lbFiles.FormattingEnabled = true;
+            this.lbFiles.Location = new System.Drawing.Point(12, 84);
+            this.lbFiles.Name = "lbFiles";
+            this.lbFiles.Size = new System.Drawing.Size(102, 199);
+            this.lbFiles.TabIndex = 3;
+            // 
+            // bLeft
+            // 
+            this.bLeft.Location = new System.Drawing.Point(135, 84);
+            this.bLeft.Name = "bLeft";
+            this.bLeft.Size = new System.Drawing.Size(33, 203);
+            this.bLeft.TabIndex = 4;
+            this.bLeft.Text = "<";
+            this.bLeft.UseVisualStyleBackColor = true;
+            this.bLeft.Click += new System.EventHandler(this.bLeft_Click);
+            // 
+            // bRight
+            // 
+            this.bRight.Location = new System.Drawing.Point(415, 84);
+            this.bRight.Name = "bRight";
+            this.bRight.Size = new System.Drawing.Size(29, 203);
+            this.bRight.TabIndex = 5;
+            this.bRight.Text = ">";
+            this.bRight.UseVisualStyleBackColor = true;
+            this.bRight.Click += new System.EventHandler(this.bRight_Click);
+            // 
+            // pbImage
+            // 
+            this.pbImage.Location = new System.Drawing.Point(174, 84);
+            this.pbImage.Name = "pbImage";
+            this.pbImage.Size = new System.Drawing.Size(235, 203);
+            this.pbImage.TabIndex = 6;
+            this.pbImage.TabStop = false;
+            this.pbImage.Click += new System.EventHandler(this.pbImage_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(12, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 16);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Files";
+            // 
+            // rb1
+            // 
+            this.rb1.AutoSize = true;
+            this.rb1.Location = new System.Drawing.Point(92, 316);
+            this.rb1.Name = "rb1";
+            this.rb1.Size = new System.Drawing.Size(31, 17);
+            this.rb1.TabIndex = 8;
+            this.rb1.TabStop = true;
+            this.rb1.Text = "1";
+            this.rb1.UseVisualStyleBackColor = true;
+            // 
+            // rb2
+            // 
+            this.rb2.AutoSize = true;
+            this.rb2.Location = new System.Drawing.Point(174, 317);
+            this.rb2.Name = "rb2";
+            this.rb2.Size = new System.Drawing.Size(31, 17);
+            this.rb2.TabIndex = 9;
+            this.rb2.TabStop = true;
+            this.rb2.Text = "2";
+            this.rb2.UseVisualStyleBackColor = true;
+            // 
+            // rb3
+            // 
+            this.rb3.AutoSize = true;
+            this.rb3.Location = new System.Drawing.Point(258, 317);
+            this.rb3.Name = "rb3";
+            this.rb3.Size = new System.Drawing.Size(31, 17);
+            this.rb3.TabIndex = 10;
+            this.rb3.TabStop = true;
+            this.rb3.Text = "3";
+            this.rb3.UseVisualStyleBackColor = true;
+            // 
+            // rb4
+            // 
+            this.rb4.AutoSize = true;
+            this.rb4.Location = new System.Drawing.Point(346, 316);
+            this.rb4.Name = "rb4";
+            this.rb4.Size = new System.Drawing.Size(31, 17);
+            this.rb4.TabIndex = 11;
+            this.rb4.TabStop = true;
+            this.rb4.Text = "4";
+            this.rb4.UseVisualStyleBackColor = true;
+            // 
+            // rb5
+            // 
+            this.rb5.AutoSize = true;
+            this.rb5.Location = new System.Drawing.Point(413, 317);
+            this.rb5.Name = "rb5";
+            this.rb5.Size = new System.Drawing.Size(31, 17);
+            this.rb5.TabIndex = 12;
+            this.rb5.TabStop = true;
+            this.rb5.Text = "5";
+            this.rb5.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(14, 317);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 16);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Mark";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(9, 346);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 16);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Comments";
+            // 
+            // tbComment
+            // 
+            this.tbComment.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbComment.Location = new System.Drawing.Point(92, 340);
+            this.tbComment.Name = "tbComment";
+            this.tbComment.Size = new System.Drawing.Size(283, 26);
+            this.tbComment.TabIndex = 15;
+            // 
+            // bAdd
+            // 
+            this.bAdd.Location = new System.Drawing.Point(381, 340);
+            this.bAdd.Name = "bAdd";
+            this.bAdd.Size = new System.Drawing.Size(63, 28);
+            this.bAdd.TabIndex = 16;
+            this.bAdd.Text = "add";
+            this.bAdd.UseVisualStyleBackColor = true;
+            this.bAdd.Click += new System.EventHandler(this.bAdd_Click);
+            // 
+            // lbComments
+            // 
+            this.lbComments.FormattingEnabled = true;
+            this.lbComments.Location = new System.Drawing.Point(92, 372);
+            this.lbComments.Name = "lbComments";
+            this.lbComments.Size = new System.Drawing.Size(283, 56);
+            this.lbComments.TabIndex = 17;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(456, 443);
+            this.Controls.Add(this.lbComments);
+            this.Controls.Add(this.bAdd);
+            this.Controls.Add(this.tbComment);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.rb5);
+            this.Controls.Add(this.rb4);
+            this.Controls.Add(this.rb3);
+            this.Controls.Add(this.rb2);
+            this.Controls.Add(this.rb1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pbImage);
+            this.Controls.Add(this.bRight);
+            this.Controls.Add(this.bLeft);
+            this.Controls.Add(this.lbFiles);
+            this.Controls.Add(this.bSearch);
+            this.Controls.Add(this.tbFolder);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Prilozhenie";
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbFolder;
+        private System.Windows.Forms.Button bSearch;
+        private System.Windows.Forms.ListBox lbFiles;
+        private System.Windows.Forms.Button bLeft;
+        private System.Windows.Forms.Button bRight;
+        private System.Windows.Forms.PictureBox pbImage;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton rb1;
+        private System.Windows.Forms.RadioButton rb2;
+        private System.Windows.Forms.RadioButton rb3;
+        private System.Windows.Forms.RadioButton rb4;
+        private System.Windows.Forms.RadioButton rb5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbComment;
+        private System.Windows.Forms.Button bAdd;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.ListBox lbComments;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Galery
+{
+    public partial class Form1 : Form
+    {
+        // List<Image> imlist = new List<Image>();
+        List<string> comments;
+        int index = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            comments = new List<string>();
+            //Serial();
+            Deserial();
+        }
+
+        private void bSearch_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+            tbFolder.Text = folderBrowserDialog1.SelectedPath.ToString();
+
+            DirectoryInfo dir = new DirectoryInfo(tbFolder.Text);
+           
+            var images = Directory.GetFiles(tbFolder.Text, "*.jpg");
+            string[] directory = Directory.GetFiles(tbFolder.Text, "*.jpg", SearchOption.TopDirectoryOnly);
+            var direc = dir.GetDirectories();
+            foreach (var item in directory)
+            {
+               // var lvi = new ListViewItem(item.Name);
+                lbFiles.Items.Add(images.ToString());
+                pbImage.Image = Image.FromFile(images[index]);
+                pbImage.SizeMode = PictureBoxSizeMode.StretchImage;
+               
+            }
+           
+           
+        }
+
+        private void pbImage_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void bRight_Click(object sender, EventArgs e)
+        {
+            var images = Directory.GetFiles(tbFolder.Text, "*.jpg");
+            if (index < images.Count()-1)
+            {
+                index++;
+                pbImage.Image = Image.FromFile(images[index]);
+                pbImage.SizeMode = PictureBoxSizeMode.StretchImage;
+              //  lbComments.Items.Clear();
+            }
+        }
+
+        private void bLeft_Click(object sender, EventArgs e)
+        {
+            var images = Directory.GetFiles(tbFolder.Text, "*.jpg");
+            if (index >0)
+            {
+                index--;
+                pbImage.Image = Image.FromFile(images[index]);
+                pbImage.SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+        }
+
+        private void bAdd_Click(object sender, EventArgs e)
+        {
+            lbComments.Items.Add(tbComment.Text);
+            tbComment.Clear();
+            comments.Add(tbComment.Text);
+        }
+
+        BinaryFormatter bw = new BinaryFormatter();
+        public void Serial()
+        {
+            using (FileStream fs = new FileStream("Comments.txt", FileMode.OpenOrCreate))
+            {
+                Serialization ob1 = new Serialization();
+                ob1.comments = comments;
+                bw.Serialize(fs, ob1);
+            }
+        }
+
+        public void Deserial()
+        {
+            using (FileStream fs = new FileStream("Comments.txt", FileMode.OpenOrCreate))
+            {
+                Serialization ob2 = (Serialization)bw.Deserialize(fs);
+                comments = ob2.comments;
+               
+            }
+        }
+    }
+
+    [Serializable]
+    class Serialization
+    {
+       public List<string> comments { get; set; }
+
+    }
+}
